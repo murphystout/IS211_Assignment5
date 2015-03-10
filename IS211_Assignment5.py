edba2893707d9f6f5867087fa21328e0109b87de
murphystout <hayes_michael_d@yahoo.com>
2015-03-10 17:04:26 -0400

    third commit

--- IS211_Assignment5.py
+++ IS211_Assignment5.py
@@ -36,6 +36,7 @@ class Server:
 	def __init__(self):
 		self.current_task = None
 		self.time_remaining = 0
+		self.queue = Queue()
 	def tick(self):
 		if self.current_task != None:
 			self.time_remaining = self.time_remaining - 1
@@ -49,6 +50,8 @@ class Server:
 	def start_next(self, new_task):
 		self.current_task = new_task
 		self.time_remaining = new_task.get_length()
+	
+	
 	
 
 class Request:
@@ -101,9 +104,60 @@ def simulateOneServer(filename):
 	print "Average Wait Time for a single server:"
 	print avg_wait_time, "seconds"
 	print avg_wait_time * 1000000, "milliseconds"
-
+	print "Number of requests:", len(waiting_times)
 	
 def simulateManyServers(filename,num_servers):
+	data = urllib2.urlopen(filename)
+	listdata = list(csv.reader(data))
+	t_list = []
+# Get first column converted in integer, and make a new list to get max value
+	for row in listdata:
+		t_list.append(int(row[0]))
+		row[0] = int(row[0])
+		row[2] = int(row[2])
+	
+	t_max = max(t_list)	
+	server_list = []
+	# Loop through num_servers to create list of server instances
+	for i in range(num_servers):
+		server_list.append(Server())
+	
+	# Attempting to give each server its own queue - request_queue = Queue()
+	waiting_times = []
+
+	t = 0
+	i = 0
+	
+	while (i < len(listdata)):
+		if(listdata[i][0] == t):
+			#Enqueue
+			#i++
+			current_time = time.time()
+			task = Request(current_time,listdata[i][2])
+			if i == 0:
+				server_iter = 0
+			else:
+				server_iter = i % num_servers # Current round-robin number
+			
+			server_list[server_iter].queue.enqueue(task)
+			i = i+1
+		else:
+			for row in server_list:
+				row.tick()
+			t = t + 1
+		for lab_server in server_list:
+			if((not lab_server.busy()) and (not lab_server.queue.is_empty())):
+				new_task = lab_server.queue.dequeue()
+				current_time = time.time()
+				waiting_times.append(new_task.wait_time(current_time))
+				lab_server.start_next(new_task)
+	avg_wait_time = sum(waiting_times)/len(waiting_times)
+	print "Average Wait Time for", num_servers, "servers"
+	print avg_wait_time, "seconds"
+	print avg_wait_time * 1000000, "milliseconds"
+	print "Number of requests:", len(waiting_times)
+	
+def simulateManyServersOneQueue(filename,num_servers):
 	data = urllib2.urlopen(filename)
 	listdata = list(csv.reader(data))
 	t_list = []
@@ -147,11 +201,12 @@ def simulateManyServers(filename,num_servers):
 	print "Average Wait Time for", num_servers, "servers"
 	print avg_wait_time, "seconds"
 	print avg_wait_time * 1000000, "milliseconds"
-	
-
 		
 if num_servers is not None:
+	print "Many Queues:"
 	simulateManyServers(url,num_servers)
+	print "One Queue:"
+	simulateManyServersOneQueue(url,num_servers)
 else:
 	simulateOneServer(url)
 	
